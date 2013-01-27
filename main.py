6d3f50b996bfb577622f2ec697220e99a8bbd941
Chris Dale <adventureonthehighseas@gmail.com>
2013-01-27 10:04:34 +0000

    Added a sound file

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def init():
   global windowSurfaceObj
   windowSurfaceObj = pygame.display.set_mode((800, 600))# pygame.FULLSCREEN)
   global background
-  background = bg.bg([0,3,1,1])
+  background = bg.bg([2,1,3,0])
   pygame.display.set_caption('Heartless Killer')
 
 
---
+++ sound.py
@@ -0,0 +1,8 @@
+import pygame, sys
+
+machineGunSound = pygame.sound.load('assets/machine-gun.wav')
+failSplatSound = pygame.sound.load('assets/fail-splat.wav')
+nastySquishSound = pygame.sound.load('assets/nasty-squish.wav')
+wetSplatSound = pygame.sound.load('assets/wet-splat.wav')
+boneSquuchSound = pygame.sound.load('assets/bone-squuch.wav')
+bodyThumpSound = pygam.sound.load('assets/thump.wav')
